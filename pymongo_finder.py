41e51bd881cdd608b3eaa154f95e1906096a1974
Roman Korolev <r.korolev@portalbilet.ru>
2025-04-29 19:13:30 +0500

    лог в тасках

--- pymongo_finder.py
+++ pymongo_finder.py
@@ -1,4 +1,21 @@
 from crawler.conf import settings
-from spiders.mongo_utils import get_bot_events
+from spiders.mongo_utils import get_mongo_bots_client
 
-print(get_bot_events())
+
+
+def get_bot_all_events():
+    if client := get_mongo_bots_client():
+        try:
+            bots_cl = client[settings.BOTS_DB][settings.EVENTS_COLLECTION]
+            return list(bots_cl.find({
+                # 'bot_status': 2,
+                # 'when': {'$gt': datetime.now()},
+                # 'url': {'$exists': True, '$nin': [None, '']},
+                # 'bot_params': {'$exists': True, '$nin': [None, {}]},
+                # 'bot_params.users': {'$exists': True, '$nin': [None, []]},
+            }))
+        except Exception as err:  # pylint: disable=W0703
+            print('Ошибка в get_bot_events:', err)
+        finally:
+            client.close()
+    return []
